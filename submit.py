425f55c1c74cbf39f6309125687ee7ad8ea4c21a
rohankarhadkar <rohankarhadkar+intellij.github@gmail.com>
2017-10-15 19:11:04 -0400

    First Commit

---
+++ submit.py
@@ -0,0 +1,19 @@
+#do not modify the function names
+#You are given L and M as input
+#Each of your functions should return the minimum possible L value alongside the marker positions
+#Or return -1,[] if no solution exists for the given L
+
+#Your backtracking function implementation
+def BT(L, M):
+    "*** YOUR CODE HERE ***"
+    return -1,[]
+
+#Your backtracking+Forward checking function implementation
+def FC(L, M):
+    "*** YOUR CODE HERE ***"
+    return -1,[]
+
+#Bonus: backtracking + constraint propagation
+def CP(L, M):
+    "*** YOUR CODE HERE ***"
+    return -1,[]
