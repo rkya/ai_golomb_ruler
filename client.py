d41c61248d96cbcb185978cdd6ab0772c37dad9a
rohankarhadkar <rohankarhadkar+intellij.github@gmail.com>
2017-10-15 23:15:59 -0400

    BT

--- client.py
+++ client.py
@@ -3,5 +3,5 @@ from submit import BT
 class Client:
 
     if __name__ == '__main__':
-        lValue, markerList = BT(4, 6)
+        lValue, markerList = BT(6, 4)
         print lValue, markerList
--- submit.py
+++ submit.py
@@ -4,9 +4,45 @@
 #Or return -1,[] if no solution exists for the given L
 
 #Your backtracking function implementation
+def selectUnassignedVariable(variables, list, csp):
+    return variables.pop()
+
+
+def isValueConsistent(value, assignedVariables, distance):
+    newSetValues = set()
+    for marker in assignedVariables:
+        newDistance = abs(value - marker)
+        if newDistance in distance or newDistance in newSetValues or newDistance == 0:
+            return 0, set()
+        newSetValues.add(newDistance)
+    distance = distance.union(newSetValues)
+    return 1, distance
+
+
+def backTrack(assignedVariables, csp, M, variables, distance):
+    if len(assignedVariables) == M:
+        return assignedVariables
+    # var = selectUnassignedVariable(variables, assignedVariables, csp)
+    for value in variables:
+        result, distance = isValueConsistent(value, assignedVariables, distance)
+        if result == 1:
+            assignedVariables.append(value)
+            assignedVariables = backTrack(assignedVariables, csp, M, variables, distance)
+            if len(assignedVariables) == M:
+                return assignedVariables
+            # assignedVariables.pop()
+            variables.append(value)
+    variables = variables.sort(reverse=True)
+    return assignedVariables
+
+
 def BT(L, M):
-    "*** YOUR CODE HERE ***"
-    return -1,[]
+    assignedVariables = list()
+    distance = set()
+    variables = [i for i in range(0, L + 1)]
+    print variables
+    assignedVariables = backTrack(assignedVariables, 0, M, variables, distance)
+    return -1, assignedVariables
 
 #Your backtracking+Forward checking function implementation
 def FC(L, M):
